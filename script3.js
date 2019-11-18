bd7b5236965bedbcf15d47114089f8309659c6a4
Mary188 <lisenki@mail.ru>
2019-11-18 18:32:37 +0300

    Третье ДЗ

---
+++ script3.js
@@ -0,0 +1,37 @@
+// 1. Заданы два массива A = [ 12, 4, 3, 10, 1, 20 ]  B = [-3, -7, -100, -33] необходимо их объединить в один массив C добавив массив B в конец(в начало) A. 
+
+// let A = [ 12, 4, 3, 10, 1, 20 ];
+// let B = [-3, -7, -100, -33];
+// let C = A.concat(B);
+// console.log(C);
+
+
+
+
+
+
+
+
+// 3. Задан массив  - [12,4,3,10,1,20]. Удалить из него наименьшее и наибольшее значение.
+
+// let arrNum = [12,4,3,10,1,20];
+// let min = arrNum[0], max = arrNum[0];
+// for(let i = 1, len = arrNum.length; i < len; i++)
+// {
+//     if(arrNum[i] > max)
+//         max = arrNum[i];
+//     if (arrNum[i] < min)
+//         min = arrNum[i];
+// }
+// console.log(min, max);
+// arrNum.sort((a, b) => a - b );
+// console.log(arrNum);
+// max = arrNum.pop();
+// min = arrNum.shift();
+// console.log(arrNum);
+
+
+
+
+
+
