198110b61a332515ed718ac6f3841c6b15c45b47
Mary188 <mariamaximycheva@gmail.com>
2019-11-25 21:43:21 +0300

    3 задача + испраления во 2

--- hw_4/script4.js
+++ hw_4/script4.js
@@ -1,13 +1,13 @@
 
 // 1. Дана строка, изображающая целое число. Вывести сумму цифр этого числа.
 
- let str = '1121231';
- let sum = 0;
- for (let i = 0; i < str.length; i++) {
-     // console.log(str[i]);
-     sum += +str[i];
- }
- console.log('Сумма цифр: ' + sum);
+//  let str = '1121231';
+//  let sum = 0;
+//  for (let i = 0; i < str.length; i++) {
+//      // console.log(str[i]);
+//      sum += +str[i];
+//  }
+//  console.log('Сумма цифр: ' + sum);
 
 
 
@@ -15,10 +15,24 @@
 // 2. Дана строка и символ. Удвоить каждое вхождение заданного символа в строку.
 
 let result = '';
-let str = 'Java Script!&&';
+let str = '$$Java Script!$$';
+let symbol = '$';
 for (let i = 0; i < str.length; i++) {
-   
-    result += str[i].repeat(2);
+    result += str[i];
+    if(str[i] === symbol){
+        result += str[i];
+    }
 }
 str = result;
 console.log(str);
+
+
+// 3 Проверить что введенный пароль удовлетворяет следующим условиям сложности:
+// - длинна от 9 символов;
+// - содержит обязательно английские буквы верхнего и нижнего регистра;
+// - содержит более двух цифр;
+// - содержит обязательно один из неалфавитных символов (например, !, $, #, %).
+
+// let pwd = prompt("Введите пароль");
+// let regExp = /(?=.*[A-Z])(?=.*[a-z])(?=.*\d.*\d.*\d)(?=.*[!$#%]){9,}/;
+// alert(regExp.test(pwd));
